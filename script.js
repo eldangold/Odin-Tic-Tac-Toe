116a97fe5f1acd1ffeef702d698d18164c4a7585
Eldan Gold <eldangold@gmail.com>
2025-06-06 17:02:16 +0200

    Add basic multiplayer support

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let cells = document.getElementById("gameboard");
 const GameBoard = (function () {
   let board = ["", "", "", "", "", "", "", "", ""];
 
@@ -10,86 +9,109 @@ const GameBoard = (function () {
 
 const displayController = (function () {
   const render = () => {
+    const gameboard = document.createElement("div");
+    gameboard.id = "gameboard";
+
     GameBoard.getBoard().forEach((value, index) => {
       let cell = document.createElement("div");
       cell.dataset.index = index;
       cell.addEventListener("click", GameController.userInput);
       cell.textContent = value;
       cell.className = "cell";
-      cells.appendChild(cell);
+      gameboard.appendChild(cell);
     });
+
+    document.getElementById("game-components").appendChild(gameboard);
   };
-  return { render };
+
+  const display = () => {
+    const displayElement = document.createElement("div");
+    displayElement.id = "display";
+    document.getElementById("game-components").appendChild(displayElement);
+  };
+  return { render, display };
 })();
 
 const GameController = (function () {
-  userMoveSymbol = "X";
-  computerMoveSymbol = "0";
+  let gameMode;
+  let activePlayer;
+
+  firstPlayerMoveSymbol = "X";
+  secondPlayerMoveSymbol = "0";
+
+  player1 = "Player 1";
+  player2 = "Player 2";
 
   const players = [
     {
-      name: "User",
-      symbol: userMoveSymbol,
+      name: player1,
+      symbol: firstPlayerMoveSymbol,
     },
     {
-      name: "Computer",
-      symbol: computerMoveSymbol,
+      name: player2,
+      symbol: secondPlayerMoveSymbol,
     },
   ];
 
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    if (gameMode === 1)
+      display.textContent = "Current player: " + activePlayer.name;
+  };
+
   const determineWinner = () => {
     if (
-      (GameBoard.getBoard()[0] == userMoveSymbol &&
-        GameBoard.getBoard()[1] == userMoveSymbol &&
-        GameBoard.getBoard()[2] == userMoveSymbol) ||
-      (GameBoard.getBoard()[3] == userMoveSymbol &&
-        GameBoard.getBoard()[4] == userMoveSymbol &&
-        GameBoard.getBoard()[5] == userMoveSymbol) ||
-      (GameBoard.getBoard()[6] == userMoveSymbol &&
-        GameBoard.getBoard()[7] == userMoveSymbol &&
-        GameBoard.getBoard()[8] == userMoveSymbol) ||
-      (GameBoard.getBoard()[0] == userMoveSymbol &&
-        GameBoard.getBoard()[3] == userMoveSymbol &&
-        GameBoard.getBoard()[6] == userMoveSymbol) ||
-      (GameBoard.getBoard()[1] == userMoveSymbol &&
-        GameBoard.getBoard()[4] == userMoveSymbol &&
-        GameBoard.getBoard()[7] == userMoveSymbol) ||
-      (GameBoard.getBoard()[2] == userMoveSymbol &&
-        GameBoard.getBoard()[5] == userMoveSymbol &&
-        GameBoard.getBoard()[8] == userMoveSymbol) ||
-      (GameBoard.getBoard()[0] == userMoveSymbol &&
-        GameBoard.getBoard()[4] == userMoveSymbol &&
-        GameBoard.getBoard()[8] == userMoveSymbol) ||
-      (GameBoard.getBoard()[2] == userMoveSymbol &&
-        GameBoard.getBoard()[4] == userMoveSymbol &&
-        GameBoard.getBoard()[6] == userMoveSymbol)
+      (GameBoard.getBoard()[0] == firstPlayerMoveSymbol &&
+        GameBoard.getBoard()[1] == firstPlayerMoveSymbol &&
+        GameBoard.getBoard()[2] == firstPlayerMoveSymbol) ||
+      (GameBoard.getBoard()[3] == firstPlayerMoveSymbol &&
+        GameBoard.getBoard()[4] == firstPlayerMoveSymbol &&
+        GameBoard.getBoard()[5] == firstPlayerMoveSymbol) ||
+      (GameBoard.getBoard()[6] == firstPlayerMoveSymbol &&
+        GameBoard.getBoard()[7] == firstPlayerMoveSymbol &&
+        GameBoard.getBoard()[8] == firstPlayerMoveSymbol) ||
+      (GameBoard.getBoard()[0] == firstPlayerMoveSymbol &&
+        GameBoard.getBoard()[3] == firstPlayerMoveSymbol &&
+        GameBoard.getBoard()[6] == firstPlayerMoveSymbol) ||
+      (GameBoard.getBoard()[1] == firstPlayerMoveSymbol &&
+        GameBoard.getBoard()[4] == firstPlayerMoveSymbol &&
+        GameBoard.getBoard()[7] == firstPlayerMoveSymbol) ||
+      (GameBoard.getBoard()[2] == firstPlayerMoveSymbol &&
+        GameBoard.getBoard()[5] == firstPlayerMoveSymbol &&
+        GameBoard.getBoard()[8] == firstPlayerMoveSymbol) ||
+      (GameBoard.getBoard()[0] == firstPlayerMoveSymbol &&
+        GameBoard.getBoard()[4] == firstPlayerMoveSymbol &&
+        GameBoard.getBoard()[8] == firstPlayerMoveSymbol) ||
+      (GameBoard.getBoard()[2] == firstPlayerMoveSymbol &&
+        GameBoard.getBoard()[4] == firstPlayerMoveSymbol &&
+        GameBoard.getBoard()[6] == firstPlayerMoveSymbol)
     ) {
       return 0;
     } else if (
-      (GameBoard.getBoard()[0] == computerMoveSymbol &&
-        GameBoard.getBoard()[1] == computerMoveSymbol &&
-        GameBoard.getBoard()[2] == computerMoveSymbol) ||
-      (GameBoard.getBoard()[3] == computerMoveSymbol &&
-        GameBoard.getBoard()[4] == computerMoveSymbol &&
-        GameBoard.getBoard()[5] == computerMoveSymbol) ||
-      (GameBoard.getBoard()[6] == computerMoveSymbol &&
-        GameBoard.getBoard()[7] == computerMoveSymbol &&
-        GameBoard.getBoard()[8] == computerMoveSymbol) ||
-      (GameBoard.getBoard()[0] == computerMoveSymbol &&
-        GameBoard.getBoard()[3] == computerMoveSymbol &&
-        GameBoard.getBoard()[6] == computerMoveSymbol) ||
-      (GameBoard.getBoard()[1] == computerMoveSymbol &&
-        GameBoard.getBoard()[4] == computerMoveSymbol &&
-        GameBoard.getBoard()[7] == computerMoveSymbol) ||
-      (GameBoard.getBoard()[2] == computerMoveSymbol &&
-        GameBoard.getBoard()[5] == computerMoveSymbol &&
-        GameBoard.getBoard()[8] == computerMoveSymbol) ||
-      (GameBoard.getBoard()[0] == computerMoveSymbol &&
-        GameBoard.getBoard()[4] == computerMoveSymbol &&
-        GameBoard.getBoard()[8] == computerMoveSymbol) ||
-      (GameBoard.getBoard()[2] == computerMoveSymbol &&
-        GameBoard.getBoard()[4] == computerMoveSymbol &&
-        GameBoard.getBoard()[6] == computerMoveSymbol)
+      (GameBoard.getBoard()[0] == secondPlayerMoveSymbol &&
+        GameBoard.getBoard()[1] == secondPlayerMoveSymbol &&
+        GameBoard.getBoard()[2] == secondPlayerMoveSymbol) ||
+      (GameBoard.getBoard()[3] == secondPlayerMoveSymbol &&
+        GameBoard.getBoard()[4] == secondPlayerMoveSymbol &&
+        GameBoard.getBoard()[5] == secondPlayerMoveSymbol) ||
+      (GameBoard.getBoard()[6] == secondPlayerMoveSymbol &&
+        GameBoard.getBoard()[7] == secondPlayerMoveSymbol &&
+        GameBoard.getBoard()[8] == secondPlayerMoveSymbol) ||
+      (GameBoard.getBoard()[0] == secondPlayerMoveSymbol &&
+        GameBoard.getBoard()[3] == secondPlayerMoveSymbol &&
+        GameBoard.getBoard()[6] == secondPlayerMoveSymbol) ||
+      (GameBoard.getBoard()[1] == secondPlayerMoveSymbol &&
+        GameBoard.getBoard()[4] == secondPlayerMoveSymbol &&
+        GameBoard.getBoard()[7] == secondPlayerMoveSymbol) ||
+      (GameBoard.getBoard()[2] == secondPlayerMoveSymbol &&
+        GameBoard.getBoard()[5] == secondPlayerMoveSymbol &&
+        GameBoard.getBoard()[8] == secondPlayerMoveSymbol) ||
+      (GameBoard.getBoard()[0] == secondPlayerMoveSymbol &&
+        GameBoard.getBoard()[4] == secondPlayerMoveSymbol &&
+        GameBoard.getBoard()[8] == secondPlayerMoveSymbol) ||
+      (GameBoard.getBoard()[2] == secondPlayerMoveSymbol &&
+        GameBoard.getBoard()[4] == secondPlayerMoveSymbol &&
+        GameBoard.getBoard()[6] == secondPlayerMoveSymbol)
     ) {
       return 1;
     } else if (GameBoard.getBoard().every((cell) => cell !== "")) return 2;
@@ -97,9 +119,9 @@ const GameController = (function () {
   };
 
   const displayWinner = () => {
-    if (determineWinner() == 0) alert("You win !");
-    else if (determineWinner() == 1) alert("You lose!");
-    else if (determineWinner() == 2) alert("It's a tie!");
+    if (determineWinner() == 0) display.textContent = player1 + " win !";
+    else if (determineWinner() == 1) display.textContent = player2 + " win!";
+    else if (determineWinner() == 2) display.textContent = "It's a tie!";
   };
 
   function computerMove() {
@@ -111,9 +133,10 @@ const GameController = (function () {
       document
         .querySelector(`[data-index="${move}"]`)
         .removeEventListener("click", userInput);
-      GameBoard.getBoard()[move] = computerMoveSymbol;
+      GameBoard.getBoard()[move] = secondPlayerMoveSymbol;
       document.querySelector(`[data-index="${move}"]`).textContent =
         GameBoard.getBoard()[move];
+      switchPlayerTurn();
       displayWinner();
     } else displayWinner();
   }
@@ -123,25 +146,44 @@ const GameController = (function () {
       document
         .querySelector(`[data-index="${e.target.dataset.index}"]`)
         .removeEventListener("click", userInput);
-      GameBoard.getBoard()[e.target.dataset.index] = userMoveSymbol;
+      GameBoard.getBoard()[e.target.dataset.index] = activePlayer.symbol;
       document.querySelector(
         `[data-index="${e.target.dataset.index}"]`
-      ).textContent = userMoveSymbol;
+      ).textContent = firstPlayerMoveSymbol;
       e.target.textContent = GameBoard.getBoard()[e.target.dataset.index];
-      computerMove();
+      switchPlayerTurn();
+      if (gameMode === 0) computerMove();
     } else displayWinner();
   };
 
-  const playGame = () => {
+  const singlePlayerPlayGame = () => {
+    gameMode = 0;
+    player1 = "You";
+    player2 = "Computer";
     GameBoard.resetBoard();
-    document.getElementById("gameboard").innerHTML = "";
+    document.getElementById("game-components").innerHTML = "";
     displayController.render();
+    displayController.display();
     computerMove();
+    activePlayer = players[0];
   };
 
-  return { playGame, userInput };
+  const multiPlayerPlayGame = () => {
+    gameMode = 1;
+    activePlayer = players[0];
+    GameBoard.resetBoard();
+    document.getElementById("game-components").innerHTML = "";
+    displayController.render();
+    displayController.display();
+  };
+
+  return { singlePlayerPlayGame, multiPlayerPlayGame, userInput };
 })();
 
 document
-  .getElementById("start-restart")
-  .addEventListener("click", () => GameController.playGame());
+  .getElementById("start-restart-singleplayer")
+  .addEventListener("click", () => GameController.singlePlayerPlayGame());
+
+document
+  .getElementById("start-restart-multiplayer")
+  .addEventListener("click", () => GameController.multiPlayerPlayGame());
